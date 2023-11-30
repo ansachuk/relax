d6c1b1937a4a15e4bdcccaea0809412e6d7f2ba6
Andrii Sachuk <ansachuk@icloud.com>
2023-11-30 18:03:18 +0200

    fix icon bug

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,6 +2,7 @@ import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher";
 import { getScopedI18n } from "@/locales/server";
 
 import css from "./Header.module.scss";
+import { Cake, Cup, Dollar, Facebook, Food, Geo, Inst, Logo, Mail, Phone } from "@/static/svg";
 
 type Props = {
 	styleProp: string;
@@ -13,6 +14,16 @@ export default async function Header({ styleProp }: Props) {
 		<div className={styleProp}>
 			<p className={css.translate}>{t("translate")}</p>
 
+			<Cake className={css.icon} width={172} height={91} />
+			<Cup className={css.icon} width={172} height={91} />
+			<Dollar className={css.icon} width={172} height={91} />
+			<Facebook className={css.icon} width={172} height={91} />
+			<Food className={css.icon} width={172} height={91} />
+			<Geo className={css.str} width={172} height={91} />
+			<Inst className={css.icon} width={172} height={91} />
+			<Mail className={css.str} width={172} height={91} />
+			<Phone className={css.icon} width={172} height={91} />
+			<Logo className={css.icon} width={172} height={91} />
 			<LanguageSwitcher />
 		</div>
 	);
