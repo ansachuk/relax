fb20d75f7ca558634bd1667c63fd82164dcfc7b9
Andrii Sachuk <ansachuk@icloud.com>
2023-11-30 17:29:08 +0200

    update icons

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher";
 import { getScopedI18n } from "@/locales/server";
-import Icon from "../Icon";
+
+import css from "./Header.module.scss";
 
 type Props = {
 	styleProp: string;
@@ -11,9 +11,8 @@ export default async function Header({ styleProp }: Props) {
 	const t = await getScopedI18n("test");
 	return (
 		<div className={styleProp}>
-			<p style={{ color: "red", fontWeight: 600 }}>{t("translate")}</p>
-			{/* <Image priority width={172} height={91} style={{ paddingTop: 14, paddingBottom: 14 }} alt="logo" src="../../static/logo.svg" /> */}
-			<Icon title="logo" width={25} height={25} />
+			<p className={css.translate}>{t("translate")}</p>
+
 			<LanguageSwitcher />
 		</div>
 	);
