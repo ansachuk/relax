a1740cf584898f651ead5c7ab3a22e207916da72
Andrii Sachuk <ansachuk@icloud.com>
2023-12-03 21:32:49 +0200

    add form elements and styles for them

--- components/Layout/LayoutForm/LayoutForm.tsx
+++ components/Layout/LayoutForm/LayoutForm.tsx
@@ -1,14 +1,39 @@
 "use client";
 
+import { FormEvent } from "react";
+
+import { useScopedI18n } from "@/locales/client";
+
+import I18nSubLayout from "@/components/I18nSubLayout/I18nSubLayout";
+
 import css from "./LayoutForm.module.scss";
+
+export function Form() {
+	const t = useScopedI18n("feedback");
+
+	const onSubmit = (e: FormEvent) => {
+		e.preventDefault();
+
+		const form = e.currentTarget;
+		console.log("form", form);
+	};
+
+	return (
+		<form onSubmit={onSubmit} className={css.form}>
+			<input type="text" name="username" placeholder={t("namePl")} />
+			<input type="tel" name="phone" placeholder={t("phonePl")} />
+			<textarea name="message" placeholder={t("messagePl")} />
+			<button className={css.submit} type="submit">
+				{t("sendPL")}
+			</button>
+		</form>
+	);
+}
 
 export default function LayoutForm() {
 	return (
-		<form className={css.form}>
-			<input type="text" />
-			<input type="text" />
-			<input type="text" />
-			<button type="submit"></button>
-		</form>
+		<I18nSubLayout>
+			<Form />
+		</I18nSubLayout>
 	);
 }
