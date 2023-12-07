eac680620b312e206575d38b054de36920dbe7eb
Andrii Sachuk <ansachuk@icloud.com>
2023-12-07 18:03:01 +0200

    finish sectionLg's

--- components/pages/main/SectionLg/SectionLg.tsx
+++ components/pages/main/SectionLg/SectionLg.tsx
@@ -1,18 +1,24 @@
+import Link from "next/link";
+
+import { getScopedI18n } from "@/locales/server";
+
+import { nothing } from "@/app/[locale]/layout";
+
+import MainImage from "@/components/MainImage/MainImage";
+
+import { ArrowLg } from "@/static/svg";
+
 import { ISectionLg } from "@/@types/types";
 
 import css from "./SectionLg.module.scss";
-import Link from "next/link";
-import { getScopedI18n } from "@/locales/server";
-import { ArrowLg } from "@/static/svg";
-import MainImage from "@/components/MainImage/MainImage";
 
 type Props = { sectionData: ISectionLg };
 
 export default async function SectionLg({
 	sectionData: {
 		images: { img1, img2, img3, img4 },
-		nothingTitle,
 		title,
+		nothingTitle,
 		subtitle,
 		phone,
 		text,
@@ -22,8 +28,8 @@ export default async function SectionLg({
 	const t = await getScopedI18n("pages.main.sectionSm");
 
 	return (
-		<article>
-			<article className={css.article}>
+		<article className={css.article}>
+			<div className={css.main}>
 				<div className={css.wrapper}>
 					<div className={css.subwrapper}>
 						<p>{subtitle}</p>
@@ -32,6 +38,9 @@ export default async function SectionLg({
 						</Link>
 					</div>
 					<p className={css.text}>{text}</p>
+				</div>
+				<div className={css.side}>
+					<h3 className={`${nothing.className} ${css.nothing}`}>{nothingTitle}</h3>
 					<Link href={`${link}#top`} className={css.link}>
 						{t("linkText")}
 						<span className={css.icon}>
@@ -39,8 +48,17 @@ export default async function SectionLg({
 						</span>
 					</Link>
 				</div>
-				{/* <MainImage width={630} height={400} src={img} alt={title} /> */}
-			</article>
+			</div>
+			<div className={css.images}>
+				<div className={css.wrapper}>
+					<MainImage width={630} height={340} src={img1} alt={title} />
+					<div className={css.subwrapper}>
+						<MainImage width={300} height={220} src={img2} alt={title} />
+						<MainImage width={300} height={220} src={img3} alt={title} />
+					</div>
+				</div>
+				<MainImage width={630} height={580} src={img4} alt={title} />
+			</div>
 		</article>
 	);
 }
