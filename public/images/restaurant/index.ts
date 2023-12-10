a533ea0cdeaf2851e4ad1fc12c5b90514843e192
Andrii Sachuk <ansachuk@icloud.com>
2023-12-11 01:21:00 +0200

    update imgs on restoran page

--- public/images/restaurant/index.ts
+++ public/images/restaurant/index.ts
@@ -19,11 +19,29 @@ export { default as menu3 } from "./menu3.webp";
 export { default as QR } from "./QR.webp";
 
 export { default as hunting1 } from "./hunting1.webp";
+export { default as hunting2 } from "./hunting2.webp";
+export { default as hunting3 } from "./hunting3.webp";
+export { default as hunting4 } from "./hunting4.webp";
 
 export { default as kolyba1 } from "./kolyba1.webp";
+export { default as kolyba2 } from "./kolyba2.webp";
+export { default as kolyba3 } from "./kolyba3.webp";
+export { default as kolyba4 } from "./kolyba4.webp";
+export { default as kolyba5 } from "./kolyba5.webp";
 
 export { default as veranda1 } from "./veranda1.webp";
+export { default as veranda2 } from "./veranda2.webp";
+export { default as veranda3 } from "./veranda3.webp";
+export { default as veranda4 } from "./veranda4.webp";
 
 export { default as venetian1 } from "./venetian1.webp";
+export { default as venetian2 } from "./venetian2.webp";
+export { default as venetian3 } from "./venetian3.webp";
+export { default as venetian4 } from "./venetian4.webp";
+export { default as venetian5 } from "./venetian5.webp";
 
 export { default as sea1 } from "./sea1.webp";
+export { default as sea2 } from "./sea2.webp";
+export { default as sea3 } from "./sea3.webp";
+export { default as sea4 } from "./sea4.webp";
+export { default as sea5 } from "./sea5.webp";
