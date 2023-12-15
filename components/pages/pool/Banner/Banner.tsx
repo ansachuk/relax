52dab94e2c0565d74362011467b5294af74178b2
Andrii Sachuk <ansachuk@icloud.com>
2023-12-15 14:32:09 +0200

    start adaptive on mainpage

--- components/pages/pool/Banner/Banner.tsx
+++
@@ -1,8 +0,0 @@
-import { getScopedI18n } from "@/locales/server";
-import css from "./Banner.module.scss";
-import { cormorant } from "@/app/[locale]/layout";
-
-export default async function Banner() {
-	const t = await getScopedI18n("pages.pool");
-	return <div className={`${cormorant.className} ${css.banner}`}>{t("bannerText")}</div>;
-}
--- public/images/layout/hero/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as main } from "./main.webp";
-export { default as hotel } from "./hotel.webp";
-export { default as restaurant } from "./restaurant.webp";
-export { default as spa } from "./spa.webp";
-export { default as pool } from "./pool.webp";
-export { default as contacts } from "./contacts.webp";
