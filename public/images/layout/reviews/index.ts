e004f0a369969ee5e4e254b55b4ac05ec17170de
Andrii Sachuk <ansachuk@icloud.com>
2023-12-19 22:53:24 +0200

    add adaptive to main components

--- public/images/layout/reviews/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as review1 } from "./review1.webp";
-export { default as review2 } from "./review2.webp";
-export { default as review3 } from "./review3.webp";
-export { default as review4 } from "./review4.webp";
