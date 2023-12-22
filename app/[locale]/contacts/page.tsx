fcd84bee94690a29829e72742553eb6b3bbfbe56
Andrii Sachuk <ansachuk@icloud.com>
2023-12-22 19:04:32 +0200

    change og photo contacts

--- app/[locale]/contacts/page.tsx
+++ app/[locale]/contacts/page.tsx
@@ -4,7 +4,7 @@ import Benefits from "@/components/pages/contacts/Benefits/Benefits";
 import Form from "@/components/pages/contacts/Form/Form";
 import Container from "@/components/Container/Container";
 
-import { getScopedI18n } from "@/locales/server";
+import { getCurrentLocale, getScopedI18n } from "@/locales/server";
 import { cormorant } from "@/public/constatnts";
 
 import css from "@/components/pages/contacts/Contacts.module.scss";
@@ -12,9 +12,11 @@ import css from "@/components/pages/contacts/Contacts.module.scss";
 // eslint-disable-next-line react-refresh/only-export-components
 export async function generateMetadata(): Promise<Metadata> {
 	const t = await getScopedI18n("navigation");
+	const locale = getCurrentLocale();
 	return {
 		title: t("contacts"),
 		openGraph: {
+			url: `https://relax-tan.vercel.app/${locale}/contacts`,
 			images: "https://relax-4mj3w3fey-ansachuks-projects.vercel.app/images/layout/og/contacts.jpg",
 		},
 	};
